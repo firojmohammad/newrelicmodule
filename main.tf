652a402c2063ca965a4806f7a532d5d36f4516a9
Firoj Mohammad <77891336+firojmohammad@users.noreply.github.com>
2024-01-19 11:51:58 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
 module "newrelic" {
   source = "./modules/newrelic"
-  count  = var.relic != null ? 1 : 0
-
-  cpu_utiliztion    = var.relic["cpu_utiliztion"]
-  memory_utiliztion = var.relic["memory_utiliztion"]
-  ecs_pending       = var.newrelic["ecs_pending"]
-  cpu_warning       = var.relic["cpu_warning"]
-  cpu_critical      = var.relic["cpu_critical"]
-  ecs_task_critical = var.relic["ecs_task_critical"]
-  alert_name_cpu    = var.relic["alert_name_cpu"]
-  alert_name_memory = var.relic["alert_name_memory"]
-  alert_ecs_task    = var.relic["alert_ecs_task"]
-  policy_name       = var.relic["policy_name"]
-  newrelic_region   = var.relic["newrelic_region"]
-  api_key           = var.relic["api_key"]
-  account_id        = var.relic["account_id"]
+  cpu_utiliztion    = var.cpu_utiliztion
+  memory_utiliztion = var.memory_utiliztion"]
+  ecs_pending       = var.ecs_pending
+  cpu_warning       = var.cpu_warning
+  cpu_critical      = var.cpu_critical
+  ecs_task_critical = var.ecs_task_critical
+  alert_name_cpu    = var.alert_name_cpu
+  alert_name_memory = var.alert_name_memory
+  alert_ecs_task    = var.alert_ecs_task
+  policy_name       = var.policy_name
+  newrelic_region   = var.newrelic_region
+  api_key           = var.api_key
+  account_id        = var.account_id
 
 
 }

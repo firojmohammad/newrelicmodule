4718d9046181652d89b3b46720d31beaefc18d26
Firoj Mohammad <77891336+firojmohammad@users.noreply.github.com>
2024-01-19 12:24:03 +0545

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,27 @@
-
+variable "cpu_utiliztion" {
+  type        = list(any)
+  description = "List of cpu_utiliztion metrics "
+}
+variable "memory_utiliztion" {
+  type        = list(any)
+  description = "List of memory_utiliztion metrics "
+}
+variable "ecs_pending" {
+  type        = list(any)
+  description = "List of ecs_pending "
+}
+variable "cpu_warning" {
+  type        = number
+  description = "cpu_warning  reservation"
+  default     = 80
+}
+variable "cpu_critical" {
+  type        = number
+  description = "cpu_critical  reservation"
+  default     = 90
+}
+variable "ecs_task_critical" {
+  type        = number
+  description = "ecs_task_critical  reservation"
+  default     = 1
+}
